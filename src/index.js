3b96fe692c1670165b6126c720e4d9667971b658
Momcilo Nikolic <momcilo.pos@gmail.com>
2024-11-21 19:55:07 +0100

    switch to render root

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client"; // Import from 'react-dom/client' in React 18
 import App from "./App";
 import { Provider } from "react-redux";
 import store from "./redux/store";
 import "./styles.css";
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
     </Provider>
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
